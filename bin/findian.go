babbc7bd5569a9614578d445fe610c9bf3a7642f
Mateo Castano <m.castano1@uniandes.edu.co>
2025-04-24 19:23:51 -0500

    findian done

--- bin/findian.go
+++ bin/findian.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	var s string
+	s := bufio.NewReader(os.Stdin)
 	fmt.Printf("Write a string:")
-	_, err := fmt.Scan(&s)
+	input, err := s.ReadString('\n')
 	if err != nil {
 		log.Printf("Error: %v", err)
 	}
-	fmt.Println(s)
+	lowerCase := strings.ToLower(input)
+	if strings.Contains(lowerCase, "i") && strings.Contains(lowerCase, "a") && strings.Contains(lowerCase, "n") {
+		fmt.Println("Found!")
+	} else {
+		fmt.Println("Not found!")
+	}
 }
